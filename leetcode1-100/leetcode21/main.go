d6dfa102acf53c8cce502dab4373ca649616c8a4
yuanjin <yuanjin@nibirutech.com>
2021-08-10 01:28:01 +0800

    21. 合并两个有序链表

---
+++ leetcode1-100/leetcode21/main.go
@@ -0,0 +1,40 @@
+package main
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	} else if l2 == nil {
+		return l1
+	}
+
+	dummy := &ListNode {
+		Val: 0,
+		Next: nil,
+	}
+
+	temp := dummy
+	for l1 != nil && l2 != nil {
+		if l1.Val <= l2.Val {
+			temp.Next = l1
+			l1 = l1.Next
+		} else {
+			temp.Next = l2
+			l2 = l2.Next
+		}	
+		temp = temp.Next
+	} 
+	if l1 == nil {
+		temp.Next = l2
+	} else {
+		temp.Next = l1
+	}
+
+	return dummy.Next
+}
