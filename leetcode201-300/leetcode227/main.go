056ef0f5be50d0db204f22765f356b7cfe11c5f5
yuanjin <yuanjin@nibirutech.com>
2021-08-22 00:26:00 +0800

    227. 基本计算器 II

---
+++ leetcode201-300/leetcode227/main.go
@@ -0,0 +1,33 @@
+package main
+
+func calculate(s string) int {
+	ans := 0
+	stack := []int{}
+	preSign := '+'
+	num := 0
+	for i, ch := range s {
+		isDigit := ch >= '0' && ch <= '9'
+		if isDigit {
+			num = num*10 + int(ch-'0')
+		}
+		if !isDigit && ch != ' ' || i == len(s) - 1 {
+			switch preSign {
+			case '+':
+				stack = append(stack, num)
+			case '-':
+				stack = append(stack, -num)
+			case '*':
+				stack[len(stack)-1] *= num 
+			default:
+				stack[len(stack)-1] /= num 
+			}
+			preSign = ch
+			num = 0
+		}
+
+	}
+	for _, v := range stack {
+		ans += v
+	}
+	return ans
+}
