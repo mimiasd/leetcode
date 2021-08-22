3b2a3a66e649af1dd6da4b61f73bf72d5ae7c655
yuanjin <yuanjin@nibirutech.com>
2021-08-22 11:59:16 +0800

    746. 使用最小花费爬楼梯

---
+++ leetcode701-800/leetcode746/main.go
@@ -0,0 +1,18 @@
+package main
+
+func minCostClimbingStairs(cost []int) int {
+	n := len(cost)
+	dp := make([]int, n+1)
+	for i := 2; i <= n; i++ {
+		dp[i] = min(dp[n-1]+cost[i-1], dp[i-2]+cost[i-2])
+	}
+
+	return dp[n]
+}
+
+func min(a, b int) int {
+    if a < b {
+        return a
+    }
+    return b
+}
